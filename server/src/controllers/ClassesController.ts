89f3a003fa292a2142f2cbfb7a12895f8b84e773
matheusEduardoTavares <matheuseduardotavaresdev@gmail.com>
2020-08-15 21:45:16 -0300

    Desenvolvido a parte de criptografia para a senha na inserção do registro da tabela users no ClassesController.ts usando o bcrypto

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 
 import convertHourToMinutes from "../utils/convertHourToMinutes"
 import db from "../database/connection"
+import bcrypt from 'bcrypt'
 
 interface ScheduleItem {
     week_day: number;
@@ -53,51 +54,61 @@ export default class ClassesController {
             cost,
             schedule
         } = request.body
-    
-        const trx = await db.transaction()
-    
-        try{
-            const insertedUsersIds = await trx('users').insert({
-                name,
-                lastname,
-                password,
-                avatar,
-                whatsapp,
-                bio
-            })
+
+        await bcrypt.hash(password, 10, async (err, hash) => {
+            if (err) throw new Error(err.toString())
+            
+            const trx = await db.transaction()
         
-            const user_id = insertedUsersIds[0]
+            try{
+                // bcrypt.compare('abcdef', hash, (err, cond) => {
+                //     console.log('OIIIIIIIIIIIIII', err, '\ncond', cond)
+                //     if (err) throw new Error(err.toString())
+                // })
+
+                const insertedUsersIds = await trx('users').insert({
+                    name,
+                    lastname,
+                    password: hash,
+                    avatar,
+                    whatsapp,
+                    bio
+                })
+            
+                const user_id = insertedUsersIds[0]
+            
+                const insertedClassesIds = await trx('classes').insert({
+                    subject,
+                    cost,
+                    user_id
+                })
+            
+                const class_id = insertedClassesIds[0]
+            
+                // const classSchedule = schedule.map((scheduleItem: any) => {
+                const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+                    return {
+                        class_id,
+                        week_day: scheduleItem.week_day,
+                        from: convertHourToMinutes(scheduleItem.from),
+                        to: convertHourToMinutes(scheduleItem.to)
+                    };
+                })
+            
+                await trx('class_schedule').insert(classSchedule)
+            
+                await trx.commit()
+            
+                return response.status(201).send()
+            }
+            catch (err) {
+                await trx.rollback()
         
-            const insertedClassesIds = await trx('classes').insert({
-                subject,
-                cost,
-                user_id
-            })
-        
-            const class_id = insertedClassesIds[0]
-        
-            // const classSchedule = schedule.map((scheduleItem: any) => {
-            const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-                return {
-                    class_id,
-                    week_day: scheduleItem.week_day,
-                    from: convertHourToMinutes(scheduleItem.from),
-                    to: convertHourToMinutes(scheduleItem.to)
-                };
-            })
-        
-            await trx('class_schedule').insert(classSchedule)
-        
-            await trx.commit()
-        
-            return response.status(201).send()
-        }
-        catch (err) {
-            await trx.rollback()
-    
-            return response.status(400).json({
-                error: 'Unexpected error while creating new class'
-            })
-        }
+                return response.status(400).json({
+                    error: 'Unexpected error while creating new class'
+                })
+            }
+
+        })
     }
 }
